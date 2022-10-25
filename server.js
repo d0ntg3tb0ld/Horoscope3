dfa3eed974075a33603812f495ec788cef6a63a1
d0ng3tb0ld <d0ng3tb0ld@proton.me>
2022-10-25 19:05:08 -0400

    taking wayy too long13

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 8000
 const date = require('date-fns')
 var format = require('date-fns/format')
 var add = require('date-fns/add')
-var newdate = format(new Date(), 'MMMM/dd/yyyy')
+var newdate = format(new Date(), 'MMMM dd yyyy')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -22,6 +22,7 @@ const zodiacs = {
         'herbs': 'Rompe Saraguey, Quita Maldicion, Pine, Tobacco, Cedarwood'
     },
     'taurus':{
+        'todays_date': newdate,
         'date_range':'April 20 - May 20',
         'planet':'Venus',
         'day':'Friday',
@@ -31,6 +32,7 @@ const zodiacs = {
         'herbs': 'Yellow Roses, Calendula Flowers, Parsley, Cinnamon, Sandalwood, Maidens Hair'
     },
     'gemini':{
+        'todays_date': newdate,
         'date_range':'May 21 - June 21',
         'planet':'Mercury',
         'day':'Wednesday',
@@ -40,6 +42,7 @@ const zodiacs = {
         'herbs': 'Abre Camino, Arrasa con todo, Oregano, Lavender, Lakespur, Lilly of the Valley'
     },
     'cancer':{
+        'todays_date': newdate,
         'date_range':'June 22 - July 23',
         'planet':'Moon',
         'day':'Monday',
@@ -49,6 +52,7 @@ const zodiacs = {
         'herbs': 'Spearmint, Lettuce, Purslane'
     },
     'leo':{
+        'todays_date': newdate,
         'date_range':'July 23 - August 22',
         'planet':'Sun',
         'day':'Sunday',
@@ -58,6 +62,7 @@ const zodiacs = {
         'herbs': 'Olibanum, Poplar Leaves, Paraiso, Cocks Comb, Geranium, Bay Leaves, Vencedor'
     },
     'virgo':{
+        'todays_date': newdate,
         'date_range':'August 23 - September 22',
         'planet':'Mercury',
         'day':'Wednesday',
@@ -67,6 +72,7 @@ const zodiacs = {
         'herbs': 'Abre Camino, Arrasa con todo, Oregano, Lavender, Lakespur, Lilly of the Valley'
     },
     'libra':{
+        'todays_date': newdate,
         'date_range':'September 23 - October 22',
         'planet':'Venus',
         'day':'Friday',
@@ -76,6 +82,7 @@ const zodiacs = {
         'herbs': 'Yellow Roses, Calendula Flowers, Parsley, Cinnamon, Sandalwood, Maidens Hair'
     },
     'scorpio':{
+        'todays_date': newdate,
         'date_range':'October 23 - November 22',
         'planet':'Mars',
         'day':'Tuesday',
@@ -85,6 +92,7 @@ const zodiacs = {
         'herbs': 'Rompe Saraguey, Quita Maldicion, Pine, Tobacco, Cedarwood'
     },
     'saggitarius':{
+        'todays_date': newdate,
         'date_range':'November 23 - December 21',
         'planet':'Jupiter',
         'day':'Thursday',
@@ -94,6 +102,7 @@ const zodiacs = {
         'herbs': 'Thuja, Anis, Honeysuckle, Oak Moss'
     },
     'capricorn':{
+        'todays_date': newdate,
         'date_range':'December 22 - January 19',
         'planet':'Saturn',
         'day':'Saturday',
@@ -103,6 +112,7 @@ const zodiacs = {
         'herbs': 'Myrrh, Cypress, Southern Leaves'
     },
     'aquarius':{
+        'todays_date': newdate,
         'date_range':'January 20 - February 18',
         'planet':'Saturn',
         'day':'Saturday',
@@ -112,6 +122,7 @@ const zodiacs = {
         'herbs': 'Myrrh, Cypress, Southern Leaves'
     },
     'pisces':{
+        'todays_date': newdate,
         'date_range':'February 19 - March 20',
         'planet':'Jupiter',
         'day':'Thursday',
@@ -121,6 +132,7 @@ const zodiacs = {
         'herbs': 'Thuja, Anis, Honeysuckle, Oak Moss'
     },
     'unknown':{
+        'todays_date': newdate,
         'date_range':'unknown',
         'planet':'unknown',
         'day':'unknown',
