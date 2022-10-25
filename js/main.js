bd09de179fce9dab7fa832cb836196409d67ea0b
d0ng3tb0ld <d0ng3tb0ld@proton.me>
2022-10-25 14:20:11 -0400

    Fixed cors errors and search errors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert('Working')
+document.querySelector('#glahh').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+  const zodiac = document.querySelector("#lopp").value.toLowerCase();
+  const res = await fetch(`https://horoscopev2-5-demo.herokuapp.com/api/${zodiacsName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#glee").textContent = data.date_range
+  document.querySelector("#gleeb").textContent = data.planet
+  document.querySelector("#gleec").textContent = data.day
+  document.querySelector("#gleed").textContent = data.color
+  document.querySelector("#gleee").textContent = data.number
+  document.querySelector("#gleef").textContent = data.gemstones
+  document.querySelector("#gleeg").textContent = data.herbs
+//   document.querySelector("#gleeh").textContent = data.todays_date
+}
