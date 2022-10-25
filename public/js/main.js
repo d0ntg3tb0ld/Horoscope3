96716d2d80d64a14307a744517460649254c4533
d0ng3tb0ld <d0ng3tb0ld@proton.me>
2022-10-25 17:48:16 -0400

    taking wayy too long6

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+document.querySelector('button').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+    const zodiac = document.querySelector("#lopp").value.toLowerCase();
+    const res = await fetch(`https://horoscopev2-5-demo.herokuapp.com/${zodiac}`)
+    const data = await res.json()
+  
+    console.log(data);
+    document.querySelector("#glee").textContent = data.date_range
+    document.querySelector("#gleeb").textContent = data.planet
+    document.querySelector("#gleec").textContent = data.day
+    document.querySelector("#gleed").textContent = data.color
+    document.querySelector("#gleee").textContent = data.number
+    document.querySelector("#gleef").textContent = data.gemstones
+    document.querySelector("#gleeg").textContent = data.herbs
+  }
